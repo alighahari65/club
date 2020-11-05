c97996a00d815a3f6bea59006a66f34a1513785c
ali90122017 <aghahari@ymail.com>
2020-11-05 13:30:38 +0330

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+@section('content')
+    @include('partials.preloader')
+    @include('partials.menu')
+    @include('partials.header')
+    @include('partials.features')
+    @include('partials.services')
+    @include('partials.special')
+    @include('partials.prices')
+    @include('partials.team')
+    @include('partials.testimonial')
+    @include('partials.faq')
+    @include('partials.clients')
+    @include('partials.blogs')
+    @include('partials.footer')
+@endsection
